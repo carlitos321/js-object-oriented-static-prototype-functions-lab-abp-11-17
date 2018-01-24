96e5781e785216870b3d62ecfce5cd6946060b8c
carlitos321 <ca.cabrera321@gmail.com>
2018-01-24 21:50:47 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -3,7 +3,7 @@ function IcebreakerResponse(email){
 }
 
 IcebreakerResponse.BatchCreate = function(data){
-  return data.map(function(responseData){
+  return data.filter(function(responseData){
     return new IcebreakerResponse(responseData[0])
   })
 }
